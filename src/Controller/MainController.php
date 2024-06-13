281e29f042c262f7c8e624fed1d500bceca4f780
MoustoifaA <moustoifa.amadi@gmail.com>
2024-06-13 12:12:50 +0200

    integration de la page home

---
+++ src/Controller/MainController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/', name: 'app_main_')]
+class MainController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    public function home(): Response
+    {
+        return $this->render('main/home.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+}
