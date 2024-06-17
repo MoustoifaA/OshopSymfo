4916a283927774017b4eecd90af128a0c56225f8
YoaN80490 <yoan.salangros@gmail.com>
2024-06-17 14:52:20 +0200

    crud des categories

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\CategoryRepository;
+use DateTimeImmutable;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
@@ -42,6 +43,7 @@ class Category
     public function __construct()
     {
         $this->products = new ArrayCollection();
+        $this->created_at = new DateTimeImmutable();
     }
 
     public function getId(): ?int
